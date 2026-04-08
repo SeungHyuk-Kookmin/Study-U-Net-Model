8bc06d5f28f3162f4fb755262cfbd3c894e84010
SeungHyuk_0276 <105186530+SeungHyuk-Kookmin@users.noreply.github.com>
2026-04-08 17:42:47 +0900

    Update dataset.py
    Add Code Comment

--- dataset.py
+++ dataset.py
@@ -7,29 +7,29 @@ import torch.nn as nn
 ## 데이터 로더를 구현하기
 class Dataset(torch.utils.data.Dataset):
     def __init__(self, data_dir, transform=None):
-        self.data_dir = data_dir
-        self.transform = transform
+        self.data_dir = data_dir        # Data 경로 설정
+        self.transform = transform      # Transform 설정
 
         lst_data = os.listdir(self.data_dir)
 
-        lst_label = [f for f in lst_data if f.startswith('label')]
-        lst_input = [f for f in lst_data if f.startswith('input')]
+        lst_label = [f for f in lst_data if f.startswith('label')]    # Label 설정
+        lst_input = [f for f in lst_data if f.startswith('input')]    # Input Data 설정
 
-        lst_label.sort()
-        lst_input.sort()
+        lst_label.sort()    # Label 정렬
+        lst_input.sort()    # Input Data 정렬
 
-        self.lst_label = lst_label
-        self.lst_input = lst_input
+        self.lst_label = lst_label    # Label 정의
+        self.lst_input = lst_input    # Input 정의
 
     def __len__(self):
-        return len(self.lst_label)
+        return len(self.lst_label) # 데이터 길이 확인
 
     def __getitem__(self, index):
         label = np.load(os.path.join(self.data_dir, self.lst_label[index]))
         input = np.load(os.path.join(self.data_dir, self.lst_input[index]))
 
-        label = label/255.0
-        input = input/255.0
+        label = label/255.0         # Pixel 정규화
+        input = input/255.0         # Pixel 정규화
 
         if label.ndim == 2:
             label = label[:, :, np.newaxis]
@@ -70,7 +70,7 @@ class Normalization(object):
 
         return data
 
-class RandomFlip(object):
+class RandomFlip(object):        # Data RandomFlip Argumentation
     def __call__(self, data):
         label, input = data['label'], data['input']
 
